f43c7b359683fba17ef5ca03b55351f59e1c2ebc
Nikolay <mystergon@gmail.com>
2015-12-24 11:26:41 +0300

    Все объединил все работает

--- Skeletal_Animation_Tutorial/src_animation/Main.cpp
+++ Skeletal_Animation_Tutorial/src_animation/Main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 #include "ogl.h"
 #include "glsl.h"
 ///////////////////////////////////////////
-vec4f lightvec(-1,0,-1,0);
+vec4f lightvec(-1, 0, -1, 0);
 #include "Mesh.h"
 ///////////////////////////////////////////
 #include "Game.h"
@@ -85,6 +85,17 @@ void Clear()
 	glLoadIdentity();
 };
 
+Monster* GetMonsterByPosition(int x, int y)
+{
+	vector<Monster*> monsters = g_game._dungeon->GetMaps()[0]->GetMonsters();
+	for (Monster* m : monsters)
+	{
+		if ((m->GetPosition().X == x) && (m->GetPosition().Y == y))
+			return m;
+	}
+	return nullptr;
+};
+
 void Begin2D(void)
 {
 	glDisable(GL_DEPTH_TEST);
@@ -120,39 +131,39 @@ void DrawQuad(float x, float y, float w, float h, uint texture)
 
 void DrawScene()
 {
-	if ( GetAsyncKeyState(VK_ESCAPE) )  exit(0);
+	if (GetAsyncKeyState(VK_ESCAPE))  exit(0);
 	// mouse pointer position
 	POINT cursor;
-	GetCursorPos(&cursor); 
+	GetCursorPos(&cursor);
 
 	// camera orientation
-	float	viewangle_x = float(cursor.x-1280/2)/4.0;
-	float	viewangle_y = float(cursor.y-768/2)/4.0;
+	float	viewangle_x = float(cursor.x - 1280 / 2) / 4.0;
+	float	viewangle_y = float(cursor.y - 768 / 2) / 4.0;
 
-	static uint t0=timeGetTime(); 
-	double time_elapsed=double(timeGetTime()-t0)/1000;
+	static uint t0 = timeGetTime();
+	double time_elapsed = double(timeGetTime() - t0) / 1000;
 
 	// clear and basic
 	glClearDepth(1);
-	glClearColor(0,0,0,1);
+	glClearColor(0, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
 
-	 //projection
+	//projection
 	int vp[4];
-	glMatrixMode( GL_PROJECTION);
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glGetIntegerv(GL_VIEWPORT, vp);
-	gluPerspective(55.0,float(vp[2])/float(vp[3]) , 0.01, 100);
+	gluPerspective(55.0, float(vp[2]) / float(vp[3]), 0.01, 100);
 
 	// modelview
-/*	glMatrixMode( GL_MODELVIEW);
+	/*	glMatrixMode( GL_MODELVIEW);
 	glLoadIdentity();
 	glRotatef( viewangle_y,1,0,0);		// set rotation
 	glRotatef( viewangle_x,0,1,0);	*/	// set rotation
-//	glTranslatef(x, y, z);
+	//	glTranslatef(x, y, z);
 	if (f)
 	{
 		//PlaySound("recycle.wav", NULL, SND_FILENAME);
@@ -169,23 +180,23 @@ void DrawScene()
 	//cam.setView();
 	// select level of detail for rendering
 	// (lods are generated automatically by the ogre xml converter )
-	int lod=0; 
+	int lod = 0;
 	/*if(GetAsyncKeyState(VK_F1)) lod=1;
 	if(GetAsyncKeyState(VK_F2)) lod=2;
 	if(GetAsyncKeyState(VK_F3)) lod=3;
 	if(GetAsyncKeyState(VK_F4)) lod=4;
 	if(GetAsyncKeyState(VK_F5)) lod=5;*/
-   /*if (GetAsyncKeyState(VK_UP))
+	/*if (GetAsyncKeyState(VK_UP))
 	{
-		cam.rotateLoc(180, 0, 1, 0);
-		cam.setView();
+	cam.rotateLoc(180, 0, 1, 0);
+	cam.setView();
 	}*/
 	g_game.cam->setView();
 	static Mesh cube("../data/cube.material", "../data/cube.xml");
 	static Mesh plane("../data/Untitled1.material", "../data/Untitled1.xml");
 	static Mesh monster("../data/SW Storm trooper.material", "../data/SW Storm trooper.mesh.xml");
 	/*static Mesh halo("../data/desktop.material",		//	required material file)
-		"../data/Desktop.mesh.xml");*/
+	"../data/Desktop.mesh.xml");*/
 	vector<Monster*> monsters = g_game._dungeon->GetMaps()[0]->GetMonsters();
 	//////////////////////
 	loopi(0, g_game._dungeon->GetMaps()[0]->GetHeight())
@@ -217,173 +228,185 @@ void DrawScene()
 		}
 	}
 
-	for(Monster* m : monsters)
-	{ 
-		monster.Draw(
-			vec3f(m->GetPosition().X*g_game._dungeon->GetMaps()[0]->GetStep() + 1.0, -0.5, m->GetPosition().Y*g_game._dungeon->GetMaps()[0]->GetStep() + 1.0),
-			vec3f(
-				0,			// rotation
-				m->GetRotation(),
-				0)
-			);
+	for (Monster* m : monsters)
+	{
+		if (!m->IsDead())
+		{
+			monster.Draw(
+				vec3f(m->GetPosition().X*g_game._dungeon->GetMaps()[0]->GetStep() + 1.0, -0.5, m->GetPosition().Y*g_game._dungeon->GetMaps()[0]->GetStep() + 1.0),
+				vec3f(
+					0,			// rotation
+					m->GetRotation(),
+					0)
+				);
+		}
 	}
 
 	/*halo.Draw(
-		vec3f(10, -0.5, 10),			// position
-		vec3f(0, 0, 0),	// rotation
-		0,							// LOD level
-		0);*/
-	
+	vec3f(10, -0.5, 10),			// position
+	vec3f(0, 0, 0),	// rotation
+	0,							// LOD level
+	0);*/
+
 
 
 	/////////////////////
 	/*cube.Draw(
-		vec3f(1.1, -0, -1),		  		// position
-		vec3f(
-			0,			// rotation
-			0,
-			0),
-		lod								// LOD level
-		);
-	
+	vec3f(1.1, -0, -1),		  		// position
+	vec3f(
+	0,			// rotation
+	0,
+	0),
+	lod								// LOD level
+	);
+
 	plane.Draw(
-		vec3f(0.1, -0, -1),		  		// position
-		vec3f(
-			0,			// rotation
-			0,
-			0),
-		lod								// LOD level
-		);*/
+	vec3f(0.1, -0, -1),		  		// position
+	vec3f(
+	0,			// rotation
+	0,
+	0),
+	lod								// LOD level
+	);*/
 	// Test 1 : Halo character (animated mesh)
 
 	/*static Mesh halo   ("../data/halo/halo.material",		//	required material file)
-						"../data/halo/halo.mesh.xml",		//	required mesh file
-						"../data/halo/halo.skeleton.xml");	//	optional skeleton file
-	
+	"../data/halo/halo.mesh.xml",		//	required mesh file
+	"../data/halo/halo.skeleton.xml");	//	optional skeleton file
+
 	loopi(0,4)
 	{
-		// Set the skeleton to an animation at a given time
+	// Set the skeleton to an animation at a given time
 
-		//int idle = halo.animation.GetAnimationIndexOf("idle"); // <- in case we dont know the animation id
-		halo.animation.SetPose(
-			i/2,			// animation id (2 animations, 0 and 1, are available)
-			time_elapsed );	// time in seconds
+	//int idle = halo.animation.GetAnimationIndexOf("idle"); // <- in case we dont know the animation id
+	halo.animation.SetPose(
+	i/2,			// animation id (2 animations, 0 and 1, are available)
+	time_elapsed );	// time in seconds
 
-		// F6 : example to manually set the shoulder - for shooting a weapon e.g.
-		if(GetAsyncKeyState(VK_F6))
-		{
-			// get the index
-			int index  = halo.animation.GetBoneIndexOf("joint2"); 
+	// F6 : example to manually set the shoulder - for shooting a weapon e.g.
+	if(GetAsyncKeyState(VK_F6))
+	{
+	// get the index
+	int index  = halo.animation.GetBoneIndexOf("joint2");
 
-			// get / modify / set the matrix
-			matrix44 m = halo.animation.bones[ index ].matrix;
-			m.x_component()=vec3f(1,0,0);
-			m.y_component()=vec3f(0,1,0); // set the rotation to identity
-			m.z_component()=vec3f(0,0,1);
-			halo.animation.bones[ index ].matrix=m;
+	// get / modify / set the matrix
+	matrix44 m = halo.animation.bones[ index ].matrix;
+	m.x_component()=vec3f(1,0,0);
+	m.y_component()=vec3f(0,1,0); // set the rotation to identity
+	m.z_component()=vec3f(0,0,1);
+	halo.animation.bones[ index ].matrix=m;
 
-			// re-evaluate the childs
-			loopi(0,halo.animation.bones[ index ].childs.size())
-			{
-				halo.animation.EvalSubtree(
-					halo.animation.bones[ index ].childs[i],	// bone id
-					halo.animation.animations[0],				// animation
-					-1);										// key frame -1 means not use the animation
-			}
-		}
-		
-		// Draw the model
+	// re-evaluate the childs
+	loopi(0,halo.animation.bones[ index ].childs.size())
+	{
+	halo.animation.EvalSubtree(
+	halo.animation.bones[ index ].childs[i],	// bone id
+	halo.animation.animations[0],				// animation
+	-1);										// key frame -1 means not use the animation
+	}
+	}
 
-		halo.Draw(
-			vec3f((i-1.5)*7,-5,7),			// position
-			vec3f(0,time_elapsed*0.3,0),	// rotation
-			lod,							// LOD level
-			(i&1)==1 );						// draw skeleton ?
+	// Draw the model
+
+	halo.Draw(
+	vec3f((i-1.5)*7,-5,7),			// position
+	vec3f(0,time_elapsed*0.3,0),	// rotation
+	lod,							// LOD level
+	(i&1)==1 );						// draw skeleton ?
 	}*/
-	
+
 	// Test 2 : moon (static mesh)
 
-	static Mesh moon   ("../data/moon/moon.material",	//	required material file)
-						"../data/moon/moon.mesh.xml");	//	required mesh file
-		moon.Draw(
-			vec3f( 5, 5, 19),		  		// position
-			vec3f(
-				time_elapsed*0.1,			// rotation
-				time_elapsed*0.1,
-				time_elapsed*0.4),	
-			lod								// LOD level
+	static Mesh moon("../data/moon/moon.material",	//	required material file)
+		"../data/moon/moon.mesh.xml");	//	required mesh file
+	moon.Draw(
+		vec3f(5, 5, 19),		  		// position
+		vec3f(
+			time_elapsed*0.1,			// rotation
+			time_elapsed*0.1,
+			time_elapsed*0.4),
+		lod								// LOD level
 		);
 
 	// Swap
 	/*static Mesh moon("../data/moon/moon.material",	//	required material file)
-		"../data/moon/moon.mesh.xml");	//	required mesh file
+	"../data/moon/moon.mesh.xml");	//	required mesh file
 	moon.Draw(
-		vec3f(1.1, -0, -1),		  		// position
-		vec3f(
-			time_elapsed*0.2,			// rotation
-			time_elapsed*0.1,
-			time_elapsed*0.4),
-		lod								// LOD level
-		);*/
+	vec3f(1.1, -0, -1),		  		// position
+	vec3f(
+	time_elapsed*0.2,			// rotation
+	time_elapsed*0.1,
+	time_elapsed*0.4),
+	lod								// LOD level
+	);*/
 
-		/*Begin2D();
-		DrawQuad(0.0f, 0.0f, 400.0f, 768.0f, 4);
-		End2D();*/
+	/*Begin2D();
+	DrawQuad(0.0f, 0.0f, 400.0f, 768.0f, 4);
+	End2D();*/
 
-		if (BtmM.isVisible)
-		{
-			BtmM.Draw();
-		}
-		if (MM.isVisible)
-		{
-			MM.Draw();
-		}
-		if (WL.isVisible)
-		{
-			WL.Draw();
-		}
+	if (BtmM.isVisible)
+	{
+		BtmM.Draw();
+	}
+	if (MM.isVisible)
+	{
+		MM.Draw();
+	}
+	if (WL.isVisible)
+	{
+		WL.Draw();
+	}
 
 	glutSwapBuffers();
-}
+};
+
 void Timer(int value)
 {
-	vector<Monster*> monsters = g_game._dungeon->GetMaps()[0]->GetMonsters();
-	int *px, *py;
-	int len;
-	for (Monster* m : monsters)
+	if (MM.isVisible)
 	{
-		if ((m->GetPosition().X != g_game.party->GetPosition().X) || (m->GetPosition().Y != g_game.party->GetPosition().Y))
+		glutTimerFunc(1000, Timer, 1);
+	}
+	else
+	{
+		vector<Monster*> monsters = g_game._dungeon->GetMaps()[0]->GetMonsters();
+		vector<PlayableCharacter*> party = g_game.party->GetCharacters();
+		int *px, *py;
+		int len;
+		for (Monster* m : monsters)
 		{
-			if (finder.lee(m->GetPosition().X, m->GetPosition().Y, g_game.party->GetPosition().X, g_game.party->GetPosition().Y, px, py, len))
+			if (((m->GetPosition().X != g_game.party->GetPosition().X) || (m->GetPosition().Y != g_game.party->GetPosition().Y)) && !(m->IsDead()))
 			{
-				/*for (int i = 0; i < len; i++)
+				if (finder.lee(m->GetPosition().X, m->GetPosition().Y, g_game.party->GetPosition().X, g_game.party->GetPosition().Y, px, py, len))
 				{
-					cout << px[i] << " " << py[i] << endl;
-				}*/
-				if (px[1] - m->GetPosition().X == 1)
-					m->SetOrientation(1, 0);
-				if (px[1] - m->GetPosition().X == -1)
-					m->SetOrientation(-1, 0);
-				if (py[1] - m->GetPosition().Y == 1)
-					m->SetOrientation(0, 1);
-				if (py[1] - m->GetPosition().Y == -1)
-					m->SetOrientation(0, -1);
-				if (((px[1] != g_game.party->GetPosition().X) || (py[1] != g_game.party->GetPosition().Y)))
-					m->SetPosition(px[1], py[1]);
+					if ((len < 9) && (!m->IsActive()))
+						m->SwitchActivity();
+
+					if ((len == 1) && (m->IsActive()))
+					{
+						m->Attack(party[rand() % 4]);
+					}
+
+					if (m->IsActive())
+					{
+						if (px[1] - m->GetPosition().X == 1)
+							m->SetOrientation(1, 0);
+						if (px[1] - m->GetPosition().X == -1)
+							m->SetOrientation(-1, 0);
+						if (py[1] - m->GetPosition().Y == 1)
+							m->SetOrientation(0, 1);
+						if (py[1] - m->GetPosition().Y == -1)
+							m->SetOrientation(0, -1);
+						if (((px[1] != g_game.party->GetPosition().X) || (py[1] != g_game.party->GetPosition().Y)))
+							m->SetPosition(px[1], py[1]);
+					}
+				}
 			}
 		}
+		if (g_game.IsOver())
+			exit(0);
+		glutPostRedisplay();
+		glutTimerFunc(1000, Timer, 1);
 	}
-	/*for (Monster* m : monsters)
-	{
-		finder.lee(m->GetPosition().X, m->GetPosition().Y, g_game.party->GetPosition().X, g_game.party->GetPosition().Y);
-		for (int i = 0; i < finder.len; i++)
-		{
-			cout << finder.px[0] << " " << finder.py[0] << endl;
-		}
-		m->SetPosition(finder.px[0], finder.py[0]);
-	}*/
-	glutPostRedisplay();
-	glutTimerFunc(1000, Timer, 1);
 }
 
 void SpecialKeys(int key, int x, int y)
@@ -391,18 +414,20 @@ void SpecialKeys(int key, int x, int y)
 	/*vec3f normal = GetLookAt();
 	int dx = 0, dy = 0;
 	if (normal.x == 1.0)
-		dx = -1;
+	dx = -1;
 	if (normal.x == -1.0)
-		dx = 1;
+	dx = 1;
 	if (normal.y == 1.0)
-		dy = -1;
+	dy = -1;
 	if (normal.y == -1.0)
-		dy = 1;
+	dy = 1;
 
 	Coordinates temp = g_game.party->GetPosition();
 	g_game.party->SetPosition(temp.X + dx, temp.Y + dy);
 
 	cout << g_game.party->GetPosition().X << " " << g_game.party->GetPosition().Y << " " << dx << " " << dy << endl;*/
+	if (MM.isVisible)
+		return;
 
 	if (key == GLUT_KEY_UP)
 	{
@@ -421,8 +446,7 @@ void SpecialKeys(int key, int x, int y)
 		g_game.party->SetPosition(temp.X + dx, temp.Y + dy);
 		vector<Monster*> monsters = g_game._dungeon->GetMaps()[0]->GetMonsters();
 		for (Monster* m : monsters)
-			throughMonster = throughMonster || checker.Check(g_game.party, m);
-		cout << g_game.party->GetPosition().X << " " << g_game.party->GetPosition().Y << " " << dx << " " << dy << endl;
+			throughMonster = throughMonster || (checker.Check(g_game.party, m) && (!m->IsDead()));
 		if (!checker.Check(g_game._dungeon->GetMaps()[0], g_game.party) && (!throughMonster))
 		{
 			g_game.cam->moveGlob(normal.x, normal.y, normal.z, -1.0);
@@ -433,7 +457,7 @@ void SpecialKeys(int key, int x, int y)
 			g_game.party->SetPosition(temp.X, temp.Y);
 		}
 	}
-	
+
 	if (key == GLUT_KEY_DOWN)
 	{
 		vec3f normal = GetLookAt();
@@ -454,7 +478,6 @@ void SpecialKeys(int key, int x, int y)
 		for (Monster* m : monsters)
 			throughMonster = throughMonster || checker.Check(g_game.party, m);
 
-		cout << g_game.party->GetPosition().X << " " << g_game.party->GetPosition().Y << " " << dx << " " << dy << endl;
 		if (!checker.Check(g_game._dungeon->GetMaps()[0], g_game.party) && (!throughMonster))
 		{
 			g_game.cam->moveGlob(normal.x, normal.y, normal.z);
@@ -475,11 +498,11 @@ void SpecialKeys(int key, int x, int y)
 		g_game.cam->rotateLoc(90, 0, 1, 0);
 
 	}
-	if(key == GLUT_KEY_PAGE_UP)
+	if (key == GLUT_KEY_PAGE_UP)
 		g_game.cam->rotateLoc(-90, 1, 0, 0);
 	if (key == GLUT_KEY_PAGE_DOWN)
 		g_game.cam->rotateLoc(90, 1, 0, 0);
-	
+
 	if (key == GLUT_KEY_F1) {
 		if (BtmM.isVisible)
 		{
@@ -527,14 +550,13 @@ void SpecialKeys(int key, int x, int y)
 		}
 	}
 
-	
 	// Обновляется окно
 	glutPostRedisplay();
 };
 
 void OtherKeys(unsigned char key, int x, int y)
 {
-	if (key == 'w') 
+	if (key == 'w')
 	{
 		yInterf += 0.001f;
 		cout << "y = " << yInterf << endl;
@@ -544,17 +566,17 @@ void OtherKeys(unsigned char key, int x, int y)
 		xInterf -= 0.001f;
 		cout << "x = " << xInterf << endl;
 	}
-	if (key == 's') 
+	if (key == 's')
 	{
 		yInterf -= 0.001f;
 		cout << "y = " << yInterf << endl;
 	}
-	if (key == 'd') 
+	if (key == 'd')
 	{
 		xInterf += 0.001f;
 		cout << "x = " << xInterf << endl;
 	}
-	if (key == 'q')
+	if (key == 'n')
 	{
 		PlaySound("../data/sounds/fight_ambient.wav", NULL, SND_ASYNC | SND_FILENAME | SND_LOOP);
 	}
@@ -568,7 +590,73 @@ void OtherKeys(unsigned char key, int x, int y)
 		cout << MM.Textures[2].height << endl;
 	}
 
+	if (key == 'x')
+	{
+		vec3f normal = GetLookAt();
+		int dx = 0, dy = 0;
+		if (normal.x == 1.0)
+			dx = -1;
+		if (normal.x == -1.0)
+			dx = 1;
+		if (normal.z == 1.0)
+			dy = -1;
+		if (normal.z == -1.0)
+			dy = 1;
+		Monster* m = GetMonsterByPosition(g_game.party->GetPosition().X + dx, g_game.party->GetPosition().Y + dy);
+		if ((!g_game.party->GetCharacters()[0]->IsDead()) && (m != nullptr))
+			g_game.party->GetCharacters()[0]->Attack(m);
+	}
 
+	if (key == 'c')
+	{
+		vec3f normal = GetLookAt();
+		int dx = 0, dy = 0;
+		if (normal.x == 1.0)
+			dx = -1;
+		if (normal.x == -1.0)
+			dx = 1;
+		if (normal.z == 1.0)
+			dy = -1;
+		if (normal.z == -1.0)
+			dy = 1;
+		Monster* m = GetMonsterByPosition(g_game.party->GetPosition().X + dx, g_game.party->GetPosition().Y + dy);
+		if ((!g_game.party->GetCharacters()[1]->IsDead()) && (m != nullptr))
+			g_game.party->GetCharacters()[1]->Attack(m);
+	}
+
+	if (key == 'v')
+	{
+		vec3f normal = GetLookAt();
+		int dx = 0, dy = 0;
+		if (normal.x == 1.0)
+			dx = -1;
+		if (normal.x == -1.0)
+			dx = 1;
+		if (normal.z == 1.0)
+			dy = -1;
+		if (normal.z == -1.0)
+			dy = 1;
+		Monster* m = GetMonsterByPosition(g_game.party->GetPosition().X + dx, g_game.party->GetPosition().Y + dy);
+		if ((!g_game.party->GetCharacters()[2]->IsDead()) && (m != nullptr))
+			g_game.party->GetCharacters()[2]->Attack(m);
+	}
+
+	if (key == 'b')
+	{
+		vec3f normal = GetLookAt();
+		int dx = 0, dy = 0;
+		if (normal.x == 1.0)
+			dx = -1;
+		if (normal.x == -1.0)
+			dx = 1;
+		if (normal.z == 1.0)
+			dy = -1;
+		if (normal.z == -1.0)
+			dy = 1;
+		Monster* m = GetMonsterByPosition(g_game.party->GetPosition().X + dx, g_game.party->GetPosition().Y + dy);
+		if ((!g_game.party->GetCharacters()[3]->IsDead()) && (m != nullptr))
+			g_game.party->GetCharacters()[3]->Attack(m);
+	}
 
 	// Обновляется окно
 	glutPostRedisplay();
@@ -921,8 +1009,8 @@ void mouseButton(int button, int state, int x, int y) {
 					WL.LoseExit.isPressed = false;
 				}
 			}
-			
-		}		
+
+		}
 	}
 	if (button == GLUT_RIGHT_BUTTON)
 	{
@@ -931,33 +1019,33 @@ void mouseButton(int button, int state, int x, int y) {
 	}
 }
 ///////////////////////////////////////////
-int main(int argc, char **argv) 
-{ 
-  glutInit(&argc, argv);  
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE /*| GLUT_ALPHA */| GLUT_DEPTH);  
-  glutInitWindowSize(WHeight, WWidth);
-  glutInitWindowPosition(0, 0);  
-  glutCreateWindow("Dungeon of Hope");
- // cam.setView();
- // Clear();
- // cam.setView();
- // Clear();
-  glutMouseFunc(mouseButton);
-  glutMotionFunc(mouseMove);
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE /*| GLUT_ALPHA */ | GLUT_DEPTH);
+	glutInitWindowSize(WHeight, WWidth);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Dungeon of Hope");
+	// cam.setView();
+	// Clear();
+	// cam.setView();
+	// Clear();
+	glutMouseFunc(mouseButton);
+	glutMotionFunc(mouseMove);
 
-  glutSpecialFunc(SpecialKeys);
-  glutKeyboardFunc(OtherKeys);
+	glutSpecialFunc(SpecialKeys);
+	glutKeyboardFunc(OtherKeys);
 
-  BtmM.InitTexture();
-  MM.InitTexture();
-  WL.InitTexture();
+	BtmM.InitTexture();
+	MM.InitTexture();
+	WL.InitTexture();
 
-  glutDisplayFunc(DrawScene);
-  glutTimerFunc(1000, Timer, 1);
-  glutIdleFunc(DrawScene);
-  glewInit();
-  wglSwapIntervalEXT(0);
-  glutMainLoop();  
-  return 0;
+	glutDisplayFunc(DrawScene);
+	glutTimerFunc(1000, Timer, 1);
+	glutIdleFunc(DrawScene);
+	glewInit();
+	wglSwapIntervalEXT(0);
+	glutMainLoop();
+	return 0;
 }
 ///////////////////////////////////////////
